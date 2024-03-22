fd9ef4a775427fd510303a5282188c06cad7089e
Tito Spadini <tito.spadini@gmail.com>
2024-03-22 14:57:57 -0300

    Adding function for converting from stereo do mono.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import soundfile as sf
 
 def read_audio(path):
@@ -11,3 +12,29 @@ def read_audio(path):
     """
     data, sampling_frequency = sf.read(path)
     return data, sampling_frequency
+
+
+def stereo_to_mono(signal, method="sum"):
+    """Converts a stereo audio signal to a mono signal.
+
+    Args:
+        signal (np.ndarray): The audio signal.
+        method (str, optional): The method to convert the signal. Defaults to "sum".
+
+    Returns:
+        np.ndarray: The mono audio signal.
+    """
+    if method == "average":
+        signal = np.mean(signal, axis=1)
+    elif method == "median":
+        signal = np.median(signal, axis=1)
+    elif method == "max":
+        signal = np.max(signal, axis=1)
+    elif method == "min":
+        signal = np.min(signal, axis=1)
+    elif method == "sum":
+        signal = np.sum(signal, axis=1)
+    else:
+        raise ValueError(f"Invalid method: {method}.")
+
+    return signal
