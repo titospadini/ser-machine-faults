a71eb8d89417b5b6e76fddb37b948f260646a247
Tito Spadini <tito.spadini@gmail.com>
2024-03-25 04:06:28 -0300

    Adding skewness and kurtosis functions.

--- features.py
+++ features.py
@@ -46,3 +46,27 @@ def median(signal):
         float: The median of the audio signal.
     """
     return np.median(signal)
+
+
+def skewness(signal):
+    """Calculates the skewness of an audio signal.
+
+    Args:
+        signal (np.ndarray): The audio signal.
+
+    Returns:
+        float: The skewness of the audio signal.
+    """
+    return (1 / len(signal)) * np.sum(((signal - mean(signal)) / standard_deviation(signal)) ** 3)
+
+
+def kurtosis(signal):
+    """Calculates the kurtosis of an audio signal.
+
+    Args:
+        signal (np.ndarray): The audio signal.
+
+    Returns:
+        float: The kurtosis of the audio signal.
+    """
+    return (1 / len(signal)) * np.sum(((signal - mean(signal)) / standard_deviation(signal)) ** 4) - 3
