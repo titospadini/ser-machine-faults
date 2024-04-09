b792f9c2c198ce1e35bb809c531f89298757dcb3
Tito Spadini <tito.spadini@gmail.com>
2024-04-09 07:41:26 -0300

    Adding energy and power functions to features.

--- features.py
+++ features.py
@@ -153,6 +153,56 @@ def kurtosis(signal, frame_length=2048, hop_length=512, center=True, pad_mode="c
     return stats.kurtosis(framed_signal, axis=-2, keepdims=True)
 
 
+def energy(signal, frame_length=2048, hop_length=512, center=True, pad_mode="constant"):
+    """ Compute the energy along the last axis.
+
+    Args:
+        signal (np.ndarray): The signal.
+        frame_length (int, optional): The frame length. Defaults to 2048.
+        hop_length (int, optional): The hop length. Defaults to 512.
+        center (bool, optional): Pad the signal by half the frame length. Defaults to True.
+        pad_mode (str, optional): The padding mode. Defaults to "constant".
+
+    Returns:
+        np.ndarray: The energy along the last axis.
+    """
+    signal = np.asarray(signal)
+
+    if center:
+        padding = [(0, 0) for _ in range(signal.ndim)]
+        padding[-1] = (int(frame_length // 2), int(frame_length // 2))
+        signal = np.pad(signal, padding, mode=pad_mode)
+
+    framed_signal = frame(signal, frame_length=frame_length, hop_length=hop_length)
+
+    return np.sum(framed_signal**2, axis=-2, keepdims=True)
+
+
+def power(signal, frame_length=2048, hop_length=512, center=True, pad_mode="constant"):
+    """ Compute the power along the last axis.
+
+    Args:
+        signal (np.ndarray): The signal.
+        frame_length (int, optional): The frame length. Defaults to 2048.
+        hop_length (int, optional): The hop length. Defaults to 512.
+        center (bool, optional): Pad the signal by half the frame length. Defaults to True.
+        pad_mode (str, optional): The padding mode. Defaults to "constant".
+
+    Returns:
+        np.ndarray: The power along the last axis.
+    """
+    signal = np.asarray(signal)
+
+    if center:
+        padding = [(0, 0) for _ in range(signal.ndim)]
+        padding[-1] = (int(frame_length // 2), int(frame_length // 2))
+        signal = np.pad(signal, padding, mode=pad_mode)
+
+    framed_signal = frame(signal, frame_length=frame_length, hop_length=hop_length)
+
+    return np.sum(framed_signal**2, axis=-2, keepdims=True) / framed_signal.shape[-2]
+
+
 def feature_extractor(signal, features, frame_length=2048, hop_length=512, center=True, pad_mode="constant"):
     """ Extract features from a signal.
 
@@ -185,6 +235,10 @@ def feature_extractor(signal, features, frame_length=2048, hop_length=512, cente
             feature_lst.append(skewness(signal, frame_length=frame_length, hop_length=hop_length, center=True).flatten())
         elif feature == "kurtosis":
             feature_lst.append(kurtosis(signal, frame_length=frame_length, hop_length=hop_length, center=True).flatten())
+        elif feature == "energy":
+            feature_lst.append(energy(signal, frame_length=frame_length, hop_length=hop_length, center=True).flatten())
+        elif feature == "power":
+            feature_lst.append(power(signal, frame_length=frame_length, hop_length=hop_length, center=True).flatten())
         else:
             raise ValueError(f"Invalid feature: {feature}.")
 
